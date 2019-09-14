4371fb371e31d4b2aa72ae2e2ffb9a41641264ad
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-09-15 00:26:17 +0200

    Add duplicate detection

--- backend/src/main/kotlin/com/frederikam/atombird/api/AccountController.kt
+++ backend/src/main/kotlin/com/frederikam/atombird/api/AccountController.kt
@@ -2,12 +2,18 @@ package com.frederikam.atombird.api
 
 import com.frederikam.atombird.data.Account
 import com.frederikam.atombird.data.AccountRepository
+import com.sun.el.parser.Token
 import org.slf4j.Logger
 import org.slf4j.LoggerFactory
+import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RestController
+import org.springframework.web.client.HttpClientErrorException
+import org.springframework.web.server.ResponseStatusException
 import reactor.core.publisher.Mono
+import reactor.core.publisher.toMono
 import java.nio.ByteBuffer
 import java.security.SecureRandom
 import java.security.spec.InvalidKeySpecException
@@ -15,7 +21,6 @@ import java.security.NoSuchAlgorithmException
 import java.util.*
 import javax.crypto.spec.PBEKeySpec
 import javax.crypto.SecretKeyFactory
-
 
 
 @RestController
@@ -29,9 +34,10 @@ class AccountController(val accounts: AccountRepository) {
     val random = SecureRandom()
 
     data class RegisterRequest(val email: String, val password: String)
+    data class TokenResponse(val token: String)
 
     @PostMapping("/register")
-    fun register(@RequestBody body: RegisterRequest): Mono<Account> {
+    fun register(@RequestBody body: RegisterRequest): Mono<TokenResponse> {
         val salt = ByteArray(saltLength)
                 .apply { random.nextBytes(this) }
                 .run { Base64.getEncoder().encodeToString(this) }
@@ -39,13 +45,20 @@ class AccountController(val accounts: AccountRepository) {
         val account = Account(body.email, salt, hash)
         account.new = true
 
-        log.info("New user account: {}", account)
-
         // TODO: Validation
-        // TODO: Check dupes
-        // TODO: Auth token
 
-        return accounts.save(account)
+        return accounts.findById(body.email)
+                .doOnNext { throw AccountAlreadyRegisteredException() }
+                .then(accounts.save(account))
+                .flatMap {
+                    log.info("New user account: {}", it.email)
+                    newTokenResponse(it)
+                }
+    }
+
+    fun newTokenResponse(account: Account): Mono<TokenResponse> {
+        // TODO
+        return Mono.just(TokenResponse("token"))
     }
 
     fun hashPassword(password: String, salt: String): String {
@@ -61,8 +74,9 @@ class AccountController(val accounts: AccountRepository) {
         } catch (e: InvalidKeySpecException) {
             throw RuntimeException(e)
         }
-
     }
 
+    class AccountAlreadyRegisteredException() :
+            ResponseStatusException(HttpStatus.BAD_REQUEST, "This account is already registered")
 
 }
