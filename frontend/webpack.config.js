3316173797c4b9a0b628854336f272846eb4fb45
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-10-07 14:58:04 +0200

    Add sass compilation

--- frontend/webpack.config.js
+++ frontend/webpack.config.js
@@ -34,7 +34,18 @@ const config = {
                         }
                     }
                 ]
-            }
+            },
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    // Creates `style` nodes from JS strings
+                    'style-loader',
+                    // Translates CSS into CommonJS
+                    'css-loader',
+                    // Compiles Sass to CSS
+                    'sass-loader',
+                ],
+            },
         ]
     },
     resolve: {
