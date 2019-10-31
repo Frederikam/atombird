6c5acbaa3396e640ac164b25a2a4ea8fdf6acceb
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-10-31 15:00:08 +0100

    Redirect logged-in users to /stream

--- frontend/src/pages/index.ts
+++ frontend/src/pages/index.ts
@@ -2,14 +2,14 @@ import {html} from "lit-html";
 import auth from "../components/auth";
 import "./index.sass"
 import {AccountStatus} from "../control/AuthManager";
+import globals from "../globals";
 
 export default (authStatus: AccountStatus | null, showRegister: boolean) => {
     if (authStatus == null) {
         return html`<h1>Unable to check login status</h1>` // TODO: Proper error handling
     } else if (authStatus.isLoggedIn) {
-        return html`
-        <h1>Logged in as ${authStatus.email}</h1>
-        `;
+        globals.router.navigate("/stream");
+        return null;
     } else {
         return html`
         <div class="index-flex">
