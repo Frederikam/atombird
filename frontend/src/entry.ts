71c6b700b9a0d642e87304c35fa83a5322243e2b
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-10-31 15:25:01 +0100

    Fix routing

--- frontend/src/entry.ts
+++ frontend/src/entry.ts
@@ -5,26 +5,55 @@ import 'normalize.css'
 import globals from "./globals"
 import auth from "./control/AuthManager"
 
-globals.router.on("/", () => {
+const requireLoggedOut = {
+    before: function (done: any, params: any) {
+        if (auth.isStatusKnown() && auth.getStatusIfKnown().isLoggedIn) {
+            done(false);
+            console.log("Navigating to /stream because we are not logged in");
+            globals.router.navigate("/stream", true)
+        } else {
+            done()
+        }
+    }
+};
+
+const requireLoggedIn = {
+    before: function (done: any, params: any) {
+        if (auth.isStatusKnown() && !auth.getStatusIfKnown().isLoggedIn) {
+            done(false);
+            console.log("Navigating to root / because we are logged out");
+            globals.router.navigate("/", true)
+        } else {
+            done()
+        }
+    }
+};
+
+globals.router.on(() => {
+    console.log("/");
     if (auth.isStatusKnown()) {
         renderLayout(index(auth.getStatusIfKnown(), true));
     } else {
         renderLayout(null);
     }
-}).on("/login", () => {
-    if (auth.isStatusKnown()) {
-        renderLayout(index(auth.getStatusIfKnown(), false));
-    } else {
-        renderLayout(null);
-    }
-}).notFound(() => {
-    renderLayout("404. Nothing here.")
-}).resolve();
+}, requireLoggedOut)
+    .on("/login", () => {
+        if (auth.isStatusKnown()) {
+            renderLayout(index(auth.getStatusIfKnown(), false));
+        } else {
+            renderLayout(null);
+        }
+    }, requireLoggedOut)
+    .notFound(() => {
+        console.log("Landed on 404 page");
+        renderLayout("404. Nothing here.")
+    })
+    .resolve();
 
 function renderLayout(body: TemplateResult | string | null) {
     render(layout(auth.getStatusIfKnown(), body), document.body);
 }
 
 auth.getStatus(status => {
-    globals.router.forceNavigate(window.location.pathname)
+    globals.router.forceNavigate(window.location.pathname, true)
 });
