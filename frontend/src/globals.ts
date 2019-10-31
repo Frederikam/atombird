909f16e7b4713f6dbd998ad041b1509208fc7e8d
Frederik Ar. Mikkelsen <fred@frederikam.com>
2019-10-31 16:02:25 +0100

    Fix router using bad root

--- frontend/src/globals.ts
+++ frontend/src/globals.ts
@@ -3,7 +3,7 @@ const Navigo = require('navigo');
 let apiBaseUrl = "http://localhost:9090/";
 
 export default {
-    router: new Navigo(),
+    router: new Navigo(location.origin),
     apiBaseUrl: apiBaseUrl,
     accountRegisterUrl: apiBaseUrl + "account/register",
     accountLoginUrl: apiBaseUrl + "account/login",
